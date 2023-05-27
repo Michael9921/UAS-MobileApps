2bd74700ccdc195756d2ea5641c6a48b3d48ada0
Michael9921 <michael.dharmawan1@student.umn.ac.id>
2023-05-27 11:51:35 +0700

    qr_code

--- Chilli/app/src/main/java/com/example/chilli/SearchGroup/SearchGroupFragment.kt
+++ Chilli/app/src/main/java/com/example/chilli/SearchGroup/SearchGroupFragment.kt
@@ -1,10 +1,14 @@
 package com.example.chilli.SearchGroup
 
+import android.content.pm.PackageManager
+import android.hardware.Camera
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.chilli.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,6 +22,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SearchGroupFragment : Fragment() {
+
+    private lateinit var codescanner: CodeScanner
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -56,5 +63,70 @@ class SearchGroupFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    if(Context.Compat.checkSelfPermission( this, Manifest.permission.CAMERA) ==
+          PackageManager.PERMISSION_DENIED){
+        ActivityCompat.requestPermissions( this, arrayof (Manifest.permission.CAMERA), requestCode: 123)
+    }else {
+        startScanning()
+    }
+
+    private fun startScanning() {
+        val scannerView: CodescannerView = findViewById(R.id.scanner_view)
+        codescanner CodeScanner( this, scannerview)
+        codescanner.camera = CodeScanner.CAMERA_BACK
+        codescanner.formats CodeScanner.ALL_FORMATS
+
+        codescanner.autoFocusMode = AutoFocusMode.SAFE
+        codescanner.scanMode = ScanMode.SINGLE
+        codescanner.isAutoFocusEnabled = true
+        codescanner.isFlashEnabled = false
+
+        codescanner.decodeCallback = DecodeCallback {
+            runonUiThread{
+                Toast.makeText(this, "Scan Result: ${it.text}", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        codescanner.errorCallback = ErrorCallback {
+            runonUiThread {
+                Toast.makeText(
+                    this,
+                    "Camera initialization error: ${it.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        scannerView.setOnClickListener {
+            codescanner.startPreview
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionResult(requestCode, permissions, grantResults)
+        if(requestCode == 123){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Camera permission granted", Toast.LENGTH_SHORT).show()
+                startScanning()
+            }else{
+                Toast.makeText(this, "Camera permission denined", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(::codescanner.isInitialized){
+            codescanner?.startPreview()
+        }
+    }
+
+    override fun onPause() {
+        if(::codescanner.isInitialized){
+            codescanner?.releaseResource()
+        }
+        super.onPause()
     }
 }
